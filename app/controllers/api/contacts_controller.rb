667f2fb356a6f39814cee6ab3ff5ab697fb8efa4
biagemc <biagemc@gmail.com>
2020-03-02 22:35:05 -0500

    add all contact + db

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -5,6 +5,18 @@ class Api::ContactsController < ApplicationController
   end
 
   def all_contacts
+    contacts = Contact.all
+    @hash_contacts = []
+
+    contacts.each do |contact|
+      @hash_contacts << {
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        email: contact.email,
+        phone_number: contact.phone_number,
+      }
+    end
+
     render "contacts.json.jb"
   end
 end
